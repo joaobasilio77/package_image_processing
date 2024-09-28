174cd518a47552d575262f71fc13f13732578120
João Basílio <146891284+joaobasilio77@users.noreply.github.com>
2024-09-28 13:57:23 -0300

    Add files via upload

---
+++ image-processing-package/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing",
+    version="0.0.1",
+    author="Joao",
+    description="Image processing package using Skimage",
+    long_description=page_description,  # Corrected from '==' to '='
+    long_description_content_type="text/markdown",
+    url="https://github.com/joaobasilio77/package_image_processing.git",  # Add your package URL here if available
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.5",
+)
